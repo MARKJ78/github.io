d4df34b7d4afb88d0a7baea29b6e9481f7ac1456
Mark Jones <mpjonesemail@gmail.com>
2016-12-28 21:33:52 +0000

    tap support

--- JS30/day1-drums/drums.js
+++ JS30/day1-drums/drums.js
@@ -7,13 +7,13 @@ function getKeydown(e){
     
 }
 
-function getClick(e){
-    console.log(e);
+/*function getClick(e){
+    //console.log(e);
     e.keyCode = e.path["1"].attributes["0"].nodeValue;
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     playSound(audio, key);
-}
+}*/
 
 function playSound(audio, key) {
     audio.currentTime = 0; //reset sound file when same key pressed
@@ -30,13 +30,15 @@ const keys = document.querySelectorAll('.key'); //select all keys
 const letter = document.querySelectorAll('kbd');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', getKeydown); 
-letter.forEach(key => key.addEventListener('click', getClick));
+//letter.forEach(key => key.addEventListener('click', getClick));
 
 
 $(function(){
   $('.key').bind('tap',tapHandler);
- 
-  function tapHandler( event ){
-     $(this).click();
-  }
+    function tapHandler( e ){
+        e.keyCode = e.currentTarget.attributes["0"].value;
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        playSound(audio, key);
+    }
 });
